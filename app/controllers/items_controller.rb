0521f7594ecafb95efe0cce8ccc7fc5b31c2289a
yamamo1984 <yytworldproducts@gmail.com>
2021-02-27 19:57:23 +0900

    出品完了時に詳細を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-       redirect_to items_path(@item)
+       redirect_to item_path(@item.id)
     else  
       render :new
     end   

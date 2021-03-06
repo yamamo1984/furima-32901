ea40a0d62e438828d50214fb6b9d6ea32206c3f4
yamamo1984 <yytworldproducts@gmail.com>
2021-03-06 15:25:14 +0900

    beforeアクションに同処理をまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :user_check, only: [:edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end  
@@ -19,18 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
-      unless current_user.id == @item.user_id then
-        redirect_to root_path
-      end  
   end  
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path(@item.id)
@@ -45,4 +41,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :detail, :condition_id, :shipping_fee_id, :shipment_place_id, :shipment_date_id, :category_id, :image).merge(user_id: current_user.id)
   end  
+
+  def set_item
+    @item = Item.find(params[:id])
+  end  
+
+  def user_check
+    unless current_user.id == @item.user_id then
+      redirect_to root_path
+    end  
+  end  
+
 end

1e331f1d6fa40f845e3ff4d2621809b5991979e9
yamamo1984 <yytworldproducts@gmail.com>
2021-05-08 12:12:11 +0900

    最新状態の更新

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,6 +2,11 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
+
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
@@ -12,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     imageElement.appendChild(blobImage);
     ImageList.appendChild(imageElement);
+
   });
 });  
 }
